5570df2876f0f6e7e4aa73b3715cf4da0f5b20da
mesut.baykal <maxutbykal41@gmail.com>
2022-09-13 17:56:49 +0300

    first_step

---
+++ src/test/java/step_defs/Hooks.java
@@ -0,0 +1,33 @@
+package step_defs;
+
+import com.demoblaze.utilities.ConfigurationReader;
+import com.demoblaze.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class Hooks {
+
+    @Before
+    public void setUp(){
+        Driver.get().get(ConfigurationReader.get("url"));
+        Driver.get().manage().window().maximize();
+        Driver.get().manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown(Scenario scenario){
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot)Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+
+        Driver.closeDriver();
+    }
+
+}
